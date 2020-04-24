07c4bb2d04cbe5d4a6f85e220e317e97044f6f63
Ubuntu <ubuntu@ip-172-31-36-63.ap-south-1.compute.internal>
2020-04-24 18:05:19 +0000

    Minor change

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Edited first time! Edited second time! Edited third time! Edited fourth time! Edited Fifth time!"
+    return "Hello World!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
